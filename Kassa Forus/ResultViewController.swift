2ed4b404beb34ef9785be21167e1a3c1ad1c2f9c
Jamal <jamal@codeweekend.nl>
2017-07-21 22:27:49 +0200

    re-organized files

--- Kassa Forus/ResultViewController.swift
+++ Kassa Forus/ResultViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class ResultViewController: UIViewController {
+class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
