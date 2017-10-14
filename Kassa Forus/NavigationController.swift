7d3abf2f06b596ddeb5bed74cd49ee31ff08507f
Jamal <jamal@stichtingforus.nl>
2017-10-14 18:29:14 +0200

    disable autorotation to avoid unexpected camera behaviour

---
+++ Kassa Forus/NavigationController.swift
@@ -0,0 +1,43 @@
+//
+//  NavigationController.swift
+//  Checkout Forus
+//
+//  Created by Jamal on 14/10/2017.
+//  Copyright © 2017 Forus. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Kassa Forus/ScannerView.swift
@@ -0,0 +1,21 @@
+//
+//  ScannerView.swift
+//  Checkout Forus
+//
+//  Created by Jamal on 14/10/2017.
+//  Copyright © 2017 Forus. All rights reserved.
+//
+
+import UIKit
+
+class ScannerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
